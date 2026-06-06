726a616a0a969b97046bfd65b14f41e788745972
Muhammad Zohaib Shahzad Sec <mzohaibshahzad.sec@gmail.com>
2026-06-06 15:12:21 -0400

    Create Comparison_IncrementLab.java

---
+++ Lab_02/task_09/Comparison_IncrementLab.java
@@ -0,0 +1,13 @@
+package lab_02.task_09;
+public class TheCurrencySwitcher{
+    public static void main(String [] args){
+        int x = 10;
+        int y = 20;
+        if (x!= y){
+            x++;
+        }
+        if (x > 10){
+            System.out.println(x);
+        }
+            }
+}
