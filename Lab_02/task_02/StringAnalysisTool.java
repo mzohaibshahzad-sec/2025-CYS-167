79ccda84f299d4cee972254b122f2515e3932db1
Muhammad Zohaib Shahzad Sec <mzohaibshahzad.sec@gmail.com>
2026-06-06 15:02:28 -0400

    Create StringAnalysisTool.java

---
+++ Lab_02/task_02/StringAnalysisTool.java
@@ -0,0 +1,9 @@
+package lab_02.task_02;
+public class StringAnalysisTool {
+    public static void main(String[]args){
+        String fullName = "Full Name" ;
+        System.out.println(fullName.length());
+        System.out.println(fullName.toUpperCase());
+        System.out.println(fullName.indexOf(" "));
+    }
+}
