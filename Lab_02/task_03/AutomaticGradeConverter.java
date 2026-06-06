549a7515a14b74a71cbc26870d9845e6aec4099a
Muhammad Zohaib Shahzad Sec <mzohaibshahzad.sec@gmail.com>
2026-06-06 15:03:41 -0400

    Create AutomaticGradeConverter.java

---
+++ Lab_02/task_03/AutomaticGradeConverter.java
@@ -0,0 +1,9 @@
+package lab_02.task_03;
+public class AutomaticGradeConverter {
+    public static void main(String[]args){
+        double percentage = 85.99;
+        int per_int = (int) percentage ;
+        System.out.println("Your raw score is: " + percentage + " but your rounded grade is:" + per_int);
+
+    }
+}
