8ecdf6cedb42e41ab971df201ff15d71522face2
Muhammad Zohaib Shahzad Sec <mzohaibshahzad.sec@gmail.com>
2026-06-07 03:47:12 -0400

    Create Std.java

---
+++ Lab_09/Students_prac/Std.java
@@ -0,0 +1,44 @@
+package Lab_09;
+interface Student {
+    void name();
+    int rollNo();
+}
+class Record implements Student {
+    @Override
+    public void name() {
+        System. out.println("Zohaib");
+    }
+    @Override
+    public int rollNo() {
+        return 167;
+    }
+}
+abstract class Teachr {
+    abstract void subject();
+}
+class Rec_01 extends Teachr implements Student {
+    @Override
+    public void subject() {
+        System. out.println("OOP");
+    }
+    @Override
+    public void name() {
+        System.out.println("Usman");
+    }
+    @Override
+    public int rollNo() {
+        return 169;
+    }
+}
+
+public class Std{
+    public static void main(String[] args) {
+        Student ab = new Record();
+        ab.name();
+        System.out.println(ab.rollNo());
+        Rec_01 obj = new Rec_01();
+        obj.subject();
+        obj.name();
+        System.out.println(obj.rollNo());
+    }
+}
