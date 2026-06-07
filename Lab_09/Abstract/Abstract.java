0ea42b1a5648c02accd56a17b6be621676b27200
Muhammad Zohaib Shahzad Sec <mzohaibshahzad.sec@gmail.com>
2026-06-07 03:23:06 -0400

    Update Abstract.java

--- Lab_09/Abstract/Abstract.java
+++ Lab_09/Abstract/Abstract.java
@@ -3,13 +3,13 @@ abstract class Students {
     abstract void name();
     abstract int rollNo();
     void display() {
-        System. out.println("Student Information");
+        System.out.println("Student Information");
     }
 }
-class ABD extends Students {
+class Detail extends Students {
     @Override
     void name() {
-        System. out.println("Muhammad Zohaib Shahzad");
+        System.out.println("Muhammad Zohaib Shahzad");
     }
     @Override
     int rollNo() {
@@ -17,7 +17,7 @@ class ABD extends Students {
 }
 public class Abstract {
     public static void main(String[] args) {
-        Students stdDetails = new ABD();
+        Students stdDetails = new Detail();
         stdDetails.name();
         System.out.println(stdDetails.rollNo());
     }
